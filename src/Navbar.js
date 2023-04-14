747585cb8b6e942056899be5e632dbbe2cc09e93
paawanEnacton <psbhatt@enacton.com>
2023-04-14 18:29:27 +0530

    connect wallet done

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,13 +2,18 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ConnectWallet } from "@thirdweb-dev/react";
 
 const Navbar = () => {
+  const handleLogout = () => {};
+
   return (
     <nav className="navbar">
       <h1> ThirdWeb Auth </h1>
       <div className="links">
         <ThirdwebProvider activeChain="mumbai">
-          <ConnectWallet theme="light" />
+          <ConnectWallet theme="light" auth={{ loginOptional: false }} />
         </ThirdwebProvider>
+        <button className="create-button" onClick={() => handleLogout}>
+          Logout
+        </button>
       </div>
     </nav>
   );
